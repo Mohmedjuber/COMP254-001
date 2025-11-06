2663e7ae07676df22c4718625b096c724ca26cff
Mohmedjuber <91512627+Mohmedjuber@users.noreply.github.com>
2025-11-06 15:58:18 -0500

    Delete MohmedJuberJangda_COMP254Lab4 directory

--- MohmedJuberJangda_COMP254Lab4/src/exercise1/Position.java
+++
@@ -1,6 +0,0 @@
-package exercise1;
-
-/** Simple Position abstraction (Goodrich-style). */
-public interface Position<E> {
-    E getElement();
-}
--- MohmedJuberJangda_COMP254Lab4/src/exercise2/Stack.java
+++
@@ -1,9 +0,0 @@
-package exercise2;
-
-public interface Stack<E> {
-    int size();
-    boolean isEmpty();
-    void push(E e);
-    E top();
-    E pop();
-}
